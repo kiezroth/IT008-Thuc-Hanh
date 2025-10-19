8e7097f96066b2d3307214eb76301ece526c6ced
kiezroth <izayoizero01@gmail.com>
2025-10-19 11:18:01 +0700

    Update Program.cs

--- BTH2_NguyenDucManh_24521042/Bai03/Program.cs
+++ BTH2_NguyenDucManh_24521042/Bai03/Program.cs
@@ -1,15 +1,62 @@
-﻿namespace Bai03
+namespace Bai03
 {
     internal class Program
     {
+        static cMatrix matr = new cMatrix();
         static void Main(string[] args)
         {
-
-            cMatrix matr = new cMatrix();
             matr.NhapMatrix();
-            matr.XuatMatrix();
-            matr.XuatSoNguyenTo();
-            matr.Find_Rows_has_MaxCountPrime();
+
+            XuatMenu();
+            int choice;
+            do
+            {
+                if (!int.TryParse(Console.ReadLine(), out choice))
+                {
+                    Console.WriteLine("Lựa chọn không hợp lệ vui lòng chọn lại: ");
+                    continue;
+                }
+
+                doWorkChoice(choice);
+            } while (choice != 0) ;
+        }
+
+        static void XuatMenu()
+        {
+            Console.WriteLine("=== Menu ===");
+            Console.WriteLine("1. Xuất ma trận");
+            Console.WriteLine("2. Tìm kiếm phần tử");
+            Console.WriteLine("3. Xuất các phần tử là số nguyên tố");
+            Console.WriteLine("4. Tìm dòng có nhiều số nguyên tố nhất");
+            Console.WriteLine("0.-- Thoát chương trình --");
+        }
+
+        static void doWorkChoice(int choice)
+        {
+            switch (choice)
+            {
+                case 1:
+                    matr.XuatMatrix();
+                    break;
+                case 2:
+                    int n = int.Parse(Console.ReadLine()!);
+                    if (matr.isFoundInMatrix(n))
+                        Console.WriteLine("Phần tử tồn tại trong ma trận");
+                    else
+                        Console.WriteLine("Phần tử không tồn tại trong ma trận");
+                    break;
+                case 3:
+                    matr.XuatSoNguyenTo();
+                    Console.WriteLine();
+                    break;
+                case 4:
+                    matr.Find_Rows_has_MaxCountPrime();
+                    break;
+                case 0:
+                    Console.WriteLine("Thoát chương trình thành công");
+                    return;
+                default: break;
+            }
         }
     }
 }
