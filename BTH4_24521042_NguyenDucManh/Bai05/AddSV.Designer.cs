03a519e46d305884b4d52e8d2401e7a8e2a0687a
kiezroth <izayoizero01@gmail.com>
2025-12-05 19:07:00 +0700

    Add files via upload

---
+++ BTH4_24521042_NguyenDucManh/Bai05/AddSV.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Bai05
+{
+    partial class AddSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.button_Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox1.Location = new System.Drawing.Point(132, 48);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(256, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox2.Location = new System.Drawing.Point(132, 106);
+            this.textBox2.MaxLength = 50;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(334, 23);
+            this.textBox2.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Khoa Công nghệ Phần mềm",
+            "Khoa Hệ thống Thông tin",
+            "Khoa Kỹ thuật Máy tính",
+            "Khoa MMT & Truyền thông",
+            "Khoa Khoa học Máy tính",
+            "Khoa Khoa học và Kỹ thuật Thông tin"});
+            this.comboBox1.Location = new System.Drawing.Point(132, 164);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(334, 25);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F);
+            this.textBox3.Location = new System.Drawing.Point(132, 222);
+            this.textBox3.MaxLength = 5;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(155, 23);
+            this.textBox3.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(117, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mã số sinh viên";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 106);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tên sinh viên";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 165);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Khoa";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 222);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Điểm TB";
+            // 
+            // button_OK
+            // 
+            this.button_OK.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button_OK.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_OK.Location = new System.Drawing.Point(283, 265);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(99, 27);
+            this.button_OK.TabIndex = 8;
+            this.button_OK.Text = "Thêm mới";
+            this.button_OK.UseVisualStyleBackColor = false;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // button_Exit
+            // 
+            this.button_Exit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button_Exit.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.button_Exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Exit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_Exit.Location = new System.Drawing.Point(388, 265);
+            this.button_Exit.Name = "button_Exit";
+            this.button_Exit.Size = new System.Drawing.Size(75, 27);
+            this.button_Exit.TabIndex = 9;
+            this.button_Exit.Text = "Thoát";
+            this.button_Exit.UseVisualStyleBackColor = false;
+            this.button_Exit.Click += new System.EventHandler(this.button_Exit_Click);
+            // 
+            // AddSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 304);
+            this.Controls.Add(this.button_Exit);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "AddSV";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Thêm Sinh Viên";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Button button_Exit;
+    }
+}
---
+++ BTH4_24521042_NguyenDucManh/Bai05/QLSV.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Bai05
+{
+    public partial class QLSV : Form
+    {
+        public QLSV()
+        {
+            InitializeComponent();
+        }
+        /// Xử lý sự kiện vẽ lại, đảm bảo image toolstripbutton dễ nhìn
+        private void toolStripButton1_Paint(object sender, PaintEventArgs e)
+        {
+            ToolStripButton button = sender as ToolStripButton;
+            if (button != null && button.Image != null)
+            {
+                Rectangle bounds = button.ContentRectangle;
+                e.Graphics.DrawImage(button.Image, bounds);
+            }
+        }
+
+        /// Chức năng thêm sinh viên mới
+        private void NewSVFunct()
+        {
+            // Tạo và hiển thị form thêm sinh viên
+            AddSV formAdd = new AddSV(); 
+            var result = formAdd.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                if (!IsPrimaryKeyUnique(formAdd.mssv))   // Kiểm tra tính duy nhất của MSSV  
+                {
+                    MessageBox.Show("MSSV đã tồn tại.", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                // Thêm dòng mới vào DataGridView
+                // Thứ tự cột: STT, MSSV, Tên, Khoa, Điểm TB
+                dataGridView1.Rows.Add(dataGridView1.RowCount, formAdd.mssv, formAdd.ten, formAdd.khoa, formAdd.dtb);
+                MessageBox.Show("Thêm mới thành công","Thông tin",MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        /// Kiểm tra tính duy nhất của MSSV
+        /// Đảm bảo mỗi sinh viên có MSSV không trùng lặp
+        private bool IsPrimaryKeyUnique(string mssv)
+        {
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue; 
+                string existingMSSV = row.Cells["MaSV"].Value.ToString();
+                if (existingMSSV == mssv) 
+                {
+                    return false;
+                }
+            }
+            return true; 
+        }
+
+        /// Lọc danh sách sinh viên theo tên (họ hoặc tên)
+        /// Chỉ hiển thị những sinh viên có tên khớp với từ khóa tìm kiếm
+        private void FilterNames(string HoTen)
+        {
+            HoTen = HoTen.ToLower();
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                if (row.IsNewRow) continue;
+                string fullName = row.Cells["TenSV"].Value.ToString();
+                string[] nameParts = fullName.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string lastName = nameParts[nameParts.Length - 1].ToLower();
+
+                if (lastName.StartsWith(HoTen)) // Kiểm tra nếu tên bắt đầu bằng từ khóa tìm kiếm
+                    row.Visible = true;
+                else 
+                    row.Visible = false;
+                
+            }
+        }
+        /// Tự động lọc danh sách khi người dùng nhập
+        private void toolStripTextBox_Search_TextChanged(object sender, EventArgs e)
+        {
+            if (toolStripTextBox_Search.Text == "")
+            {
+                // Nếu ô tìm kiếm trống, hiển thị tất cả sinh viên
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    if (row.IsNewRow) continue;
+                    row.Visible = true;
+                }
+            }
+            else
+            {
+                // Nếu có từ khóa, thực hiện lọc theo tên
+                FilterNames(toolStripTextBox_Search.Text);
+            }
+        }
+        private void toolStripButton_New_Click(object sender, EventArgs e)
+        {
+            NewSVFunct();
+        }
+        private void ToolStripMenuItem_Add_Click(object sender, EventArgs e)
+        {
+            NewSVFunct();
+        }
+        private void ToolStripMenuItem_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
